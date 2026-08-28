699a543d4fde5038d9e23f687e1db288caae7bb9
Callmhetcp <dannywiz480@gmail.com>
2026-08-28 10:52:00 +0100

    Configure Vue frontend foundation

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="min-h-screen flex items-center justify-center">
+        <h1 class="text-3xl font-bold">
+            Simple Chat
+        </h1>
+    </main>
+</template>
